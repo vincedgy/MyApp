bc2f531cb490af1c9f74ecb82fffdb06119b3b57
vincedgy <vincent.dagoury@gmail.com>
2014-11-11 12:54:06 +0100

    Fixing relative path

--- pack.cmd
+++ pack.cmd
@@ -1,1 +1,3 @@
-7z a -ttar ../MyApp.tar .\* -x!node_modules -x!static\components -x!.idea -x!.git 
+del /y ..\MyApp.tar.bak
+rename ..\MyApp.tar ..\MyApp.tar.bak
+7z a -ttar ..\MyApp.tar * -x!node_modules -x!static\components -x!.git -x!.idea -x!*.log
